f7e5e7515f425d2a52a9a1b4aa823a156369e394
fergcb <fergus.bentley@gmail.com>
2022-07-17 16:09:37 +0100

    Add "at" clause to LOOK command

--- src/main/java/uk/fergcb/rogue/parser/commands/LookCommand.java
+++ src/main/java/uk/fergcb/rogue/parser/commands/LookCommand.java
@@ -2,6 +2,8 @@ package uk.fergcb.rogue.parser.commands;
 
 import uk.fergcb.rogue.Interaction;
 import uk.fergcb.rogue.InteractionType;
+import uk.fergcb.rogue.Text;
+import uk.fergcb.rogue.entities.Entity;
 import uk.fergcb.rogue.entities.actors.Actor;
 import uk.fergcb.rogue.parser.ParseResult;
 import uk.fergcb.rogue.parser.Parser;
@@ -9,15 +11,34 @@ import uk.fergcb.rogue.parser.combinators.Sequence;
 
 import java.util.List;
 
-import static uk.fergcb.rogue.parser.Parsers.str;
+import static uk.fergcb.rogue.parser.Parsers.*;
 
 public class LookCommand extends Command {
 
-    Parser parser = Sequence.of(str("look"));
+    Parser parser = Sequence.of(str("look"), opt(Sequence.of(str("at"), phrase)));
 
     @Override
     protected Interaction resolve(String command, List<String> args, Actor actor) {
-        return new Interaction(InteractionType.LOOK, actor, actor);
+        if (args.size() == 0)
+            return new Interaction(InteractionType.LOOK, actor, actor);
+
+        String targetName = Entity.stripArticle(args.get(1));
+        List<Entity> targets = actor.currentRoom.findEntity(targetName);
+
+        if (targets.size() == 0)
+            return Interaction.fail("I can't see a " + Text.red(targetName) + ".");
+
+        if (targets.size() == 1) {
+            Entity target = targets.get(0);
+            return new Interaction(InteractionType.LOOK, actor, target);
+        }
+
+        List<String> options = targets
+                .stream()
+                .map(Entity::getName)
+                .toList();
+
+        return Interaction.clarify(targetName, options);
     }
 
     @Override
